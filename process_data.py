3fe692c7f5de4de9b46ae9a82fda8c8daf279882
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2023-11-20 16:04:02 -0500

    Create basic data processor

---
+++ process_data.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas.errors
+from IPython.display import display
+from tqdm import tqdm
+
+
+def load_data(
+    filepath, headers=["prev", "curr", "type", "n"], sep="\t", bad_lines="warn"
+):
+    """
+    Loads clickstream data and performs basic cleaning (lowercasing and dropping NaNs).
+    :param bad_lines: Defines what to do when encountering a line with too many fields
+    :type bad_lines: str (default: "warn")
+    :param sep: Delimiter in file.
+    :type sep: str
+    :param headers: Column headers for file.
+    :type headers: list (default: clickstream headers)
+    :param filepath: Path to file.
+    :type filepath: str
+    :return: Cleaned data loaded from given file.
+    :rtype: pandas dataframe
+    """
+    # TODO: Consider handling spaces represented with _
+    clickstream = pd.read_csv(filepath, sep=sep, names=headers, on_bad_lines=bad_lines)
+    cols = clickstream.columns
+    for col in tqdm(cols, desc="Processing text"):
+        try:
+            if clickstream[col].dtype == "O":
+                clickstream.loc[:, col] = clickstream[col].str.lower()
+        except AttributeError as e:
+            print(f"Check for non-string datatypes: {e}")
+    clickstream.dropna(inplace=True)
+    return clickstream
+
+
+def filter_pages(data, page="2022_Russian_invasion_of_Ukraine", column="curr"):
+    """
+    Returns a subset of clickstreams for the specified Wikipedia article.
+    :param data: Clickstream data.
+    :type data: pandas Dataframe
+    :param page: Title of Wikipedia page.
+    :type page: str
+    :param column: Name of column to search for matches.
+    :type column: str
+    :return: Subset of clickstream data matching desired page.
+    :rtype: pandas Dataframe
+    """
+    filtered = data[data[column] == page.lower()]
+    return filtered
+
+
+def visualize_n(data, x="prev", y="n"):
+    """
+    Creates matplotlib bar plot for clickstreams
+    :param y: Column name with values for the y-axis.
+    :type y: str
+    :param x: Column name with values for the x-axis.
+    :type x: str
+    :param data: Clickstream data.
+    :type data: pandas Dataframe
+    :return: Plot of values
+    :rtype: Matplotlib object
+    """
+    plt.figure(figsize=(12, 12))
+    viz = plt.bar(data[x], data[y])
+    return viz
+
+
+def main():
+    ua_clicks202203 = load_data("./data/clickstream-enwiki-2022-03.tsv")
+    filtered_uaclicks = filter_pages(ua_clicks202203)
+    display(filtered_uaclicks.sort_values(by="n", axis=0, ascending=False)[:20])
+    display(filtered_uaclicks.describe())
+
+    # Plot top 50 clicks to "2022 Russian Invasion of Ukraine" Wiki Page
+    visualize_n(filtered_uaclicks.sort_values(by="n", axis=0, ascending=False)[:50])
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
+    print("Code execution complete")
+
+
+if __name__ == "__main__":
+    main()
